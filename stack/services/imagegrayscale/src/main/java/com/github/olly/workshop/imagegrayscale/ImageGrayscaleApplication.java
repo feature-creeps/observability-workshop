2576a8522e8d8fb81d0caa68fb1bbefed2afad36
Jon Barber <jon.barber@acm.org>
2019-02-01 14:51:14 +0000

    Image Grayscale Service  (#3)
    * Initial project import
    
    * No-op controller to accept grayscale requests
    
    * Renamed group in-line with others
    
    * Convert an image to grayscale
    
    * Renamed in line with the `imagerotator` service
    
    * Use the gradle-docker plugin to create a docker image
    
    * Add Dockerfile so docker compose for stacks can work
    
    * Add the image grayscale service to the local stack

---
+++ stack/services/imagegrayscale/src/main/java/com/github/olly/workshop/imagegrayscale/ImageGrayscaleApplication.java
@@ -0,0 +1,14 @@
+package com.github.olly.workshop.imagegrayscale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageGrayscaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageGrayscaleApplication.class, args);
+	}
+
+}
+
