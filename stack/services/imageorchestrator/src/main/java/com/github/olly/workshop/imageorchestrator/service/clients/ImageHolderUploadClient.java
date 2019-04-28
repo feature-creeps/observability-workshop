55126c28f39a76c71b6a937e0a9dea8646be91ea
Jon Barber <jon.barber@acm.org>
2019-04-28 17:45:41 +0100

    Use RestTemplate when saving images so that ZipKin spans are propagated across service calls

--- stack/services/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/service/clients/ImageHolderUploadClient.java
+++ stack/services/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/service/clients/ImageHolderUploadClient.java
@@ -2,41 +2,49 @@ package com.github.olly.workshop.imageorchestrator.service.clients;
 
 
 import com.github.olly.workshop.imageorchestrator.model.Image;
-import org.apache.commons.io.IOUtils;
-import org.apache.http.HttpEntity;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.ContentType;
-import org.apache.http.entity.mime.MultipartEntityBuilder;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-
-import java.io.IOException;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ImageHolderUploadClient {
 
+    @Autowired
+    RestTemplate restTemplate;
+
+
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageHolderUploadClient.class);
 
     public void upload(Image image) {
-        CloseableHttpClient httpClient = HttpClients.createDefault();
-        HttpPost uploadFile = new HttpPost("http://imageholder:8080/api/images");
-        uploadFile.setEntity(imageToMultipart(image));
-        CloseableHttpResponse response = null;
-        try {
-            response = httpClient.execute(uploadFile);
-            LOGGER.info("Successfully uploaded transformed image to the imageholder with id {}", IOUtils.toString(response.getEntity().getContent()));
-        } catch (IOException e) {
-            LOGGER.error("Failed uploading the transformed image to the imageholder", e);
-        }
+
+        MultiValueMap<String, String> fileMap = new LinkedMultiValueMap<>();
+        ContentDisposition contentDisposition = ContentDisposition
+                .builder("form-data")
+                .name("image")
+                .filename("image")
+                .build();
+        fileMap.add(HttpHeaders.CONTENT_DISPOSITION, contentDisposition.toString());
+        fileMap.add(HttpHeaders.CONTENT_TYPE, image.getMimeType());
+        org.springframework.http.HttpEntity<byte[]> fileEntity = new org.springframework.http.HttpEntity<>(image.getData(), fileMap);
+
+        MultiValueMap<String, Object> theMulitpartRequest = new LinkedMultiValueMap<>();
+
+        theMulitpartRequest.add("image", fileEntity);
+
+
+        HttpHeaders theMultipartHeaders = new HttpHeaders();
+        theMultipartHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        org.springframework.http.HttpEntity<MultiValueMap<String, Object>> requestEntity = new org.springframework.http.HttpEntity<>(theMulitpartRequest, theMultipartHeaders);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://imageholder:8080/api/images", HttpMethod.POST, requestEntity, String.class);
+
+        LOGGER.info("Successfully uploaded transformed image to the imageholder with id {}", response.getBody());
+
     }
 
-    private HttpEntity imageToMultipart(Image image) {
-        MultipartEntityBuilder builder = MultipartEntityBuilder.create();
-        builder.addBinaryBody("image", image.getData(), ContentType.create(image.getMimeType()), "image");
-        return builder.build();
-    }
 }
