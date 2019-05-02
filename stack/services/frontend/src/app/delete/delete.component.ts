eb0d36b7e666a40a8f5183ee179359e62c541ab1
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2019-05-02 18:49:48 +0200

    deletion form works

--- stack/services/frontend/src/app/delete/delete.component.ts
+++ stack/services/frontend/src/app/delete/delete.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
@@ -6,6 +6,7 @@ import {HttpClient} from "@angular/common/http";
   templateUrl: './delete.component.html',
   styleUrls: ['./delete.component.css']
 })
+
 export class DeleteComponent implements OnInit {
 
   constructor(private http: HttpClient) {
@@ -15,16 +16,51 @@ export class DeleteComponent implements OnInit {
     this.retrieveImages();
   }
 
-  public data;
+  public ids;
   public images;
   public displayImage;
+  public deleteId: string;
+
+  async deleteOne() {
+    let res;
+    try {
+      res = await this.http.delete('http://localhost:8080/api/images/' + this.deleteId, {responseType: 'Blob'}).toPromise();
+    } catch (e) {
+      console.log(e)
+    }
+    if (res != null) {
+      console.log("image deleted " + this.deleteId);
+      document.getElementById("info").innerText = "Deleted " + this.deleteId;
+      this.retrieveImages();
+    }
+  }
+
+  async deleteAll() {
+    let res;
+    try {
+      res = await this.http.post('http://localhost:8080/api/images/delete/all').toPromise();
+    } catch (e) {
+      console.log(e)
+      // todo: deletion works but somehow response fails
+      console.log("Deleted all images");
+      document.getElementById("info").innerText = "Deleted all images";
+    }
+    if (res != null) {
+      console.log("Deleted all images");
+      document.getElementById("info").innerText = "Deleted all images";
+    }
+  }
 
   async retrieveImages() {
     let data = await this.http.get<Array<Image>>('http://localhost:8080/api/images').toPromise();
     if (data.length > 0) {
+      document.getElementById("preview").hidden = false;
       this.images = data;
       this.setIds(data);
       this.showImage(this.images[0].id);
+    } else {
+      document.getElementById("info").innerText = "No images found";
+      document.getElementById("preview").hidden = true;
     }
   }
 
@@ -33,7 +69,7 @@ export class DeleteComponent implements OnInit {
     for (var i = 0; i < data.length; i++) {
       list.push(data[i].id)
     }
-    this.data = list;
+    this.ids = list;
     return list;
   }
 
@@ -41,6 +77,7 @@ export class DeleteComponent implements OnInit {
     let data = await this.http.get('http://localhost:8080/api/images/' + id, {responseType: 'blob'}).toPromise();
     if (data != null) {
       this.displayImage = this.createImageFromBlob(data);
+      this.deleteId = id;
     }
   }
 
--- stack/services/frontend/todo.md
+++ stack/services/frontend/todo.md
@@ -1,9 +1,17 @@
+### todo
+* set up environments to support backend service configuration
+* set up nginx
+* set up nginx to act as an api proxy
 * redirect to display page after uploading a new image / or give any feedback
 * style text next to upload buttons
 * show transformed image after orchestration
-* fix delete form
 * fix orchestrate form
 * propagating the name in upload does not work
 * generic orchestrator frontend (plug and play image manipulations)
 * add an album view
 * display button/id towards particular image in random component
+* show nice info if there are now images (random, display, delete, orchestrate)
+* make delete all button more responsive and fix the "error response in the console"
+
+### done
+* fix delete form
