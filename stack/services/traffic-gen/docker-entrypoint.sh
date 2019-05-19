ad842ee3f6149e6d43dda2a6c9adad5dba7358dc
Jon Barber <jon.barber@acm.org>
2019-05-19 11:48:45 +0100

    Refactored traffic generator Dockerfile
    Binaries pulled now in multi-stage docker build rather than at runtime, resulting in more predictable runs

--- stack/services/traffic-gen/docker-entrypoint.sh
+++ stack/services/traffic-gen/docker-entrypoint.sh
@@ -3,19 +3,6 @@ set -e
 
 cd /tmp
 
-wget -q https://github.com/tsenart/vegeta/releases/download/cli%2Fv12.4.0/vegeta-12.4.0-linux-amd64.tar.gz
-
-tar xzvf vegeta-12.4.0-linux-amd64.tar.gz
-chmod a+x vegeta
-mv vegeta /usr/local/bin
-
-echo "Fetching upload traffic generator"
-
-wget -q https://github.com/feature-creeps/upload-traffic-gen/releases/download/v0.4.0/upload-traffic-gen_0.4.0_Linux_x86_64.tar.gz
-tar xzvf upload-traffic-gen_0.4.0_Linux_x86_64.tar.gz
-chmod a+x upload-traffic-gen
-mv upload-traffic-gen /usr/local/bin
-
 echo "Waiting for imgeholder service to come up"
 
 until $(curl --output /dev/null --silent --head --fail http://imageholder:8080/actuator/health); do
@@ -32,17 +19,9 @@ echo "imageholder running - uploading images"
 
 echo "Uploaded images"
 
-
-echo "Fetching transformation traffic generator"
-
-wget -q https://github.com/feature-creeps/transform-traffic-gen/releases/download/v0.2.0/transform-traffic-gen_0.2.0_Linux_x86_64.tar.gz
-tar xzvf transform-traffic-gen_0.2.0_Linux_x86_64.tar.gz
-chmod a+x transform-traffic-gen
-mv transform-traffic-gen /usr/local/bin
-
 echo "Waiting for imageorchestrator service to come up"
 
-until $(curl --output /dev/null --silent --head --fail http://imageholder:8080/actuator/health); do
+until $(curl --output /dev/null --silent --head --fail http://imageorchestrator:8080/actuator/health); do
       printf '.'
       sleep 5
 done
