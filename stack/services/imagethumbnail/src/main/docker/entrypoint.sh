39290e57b795f50836f665918c34abe9d29f9bc7
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2019-10-23 00:33:43 +0200

    fixed apm server url

--- stack/services/imagethumbnail/src/main/docker/entrypoint.sh
+++ stack/services/imagethumbnail/src/main/docker/entrypoint.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 JAVA_OPTIONS=""
 JAVA_OPTIONS="$JAVA_OPTIONS -Djava.security.egd=file:/dev/./urandom"
+JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.service_name=imagethumbnail"
+JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.server_urls=http://apm-server:8200"
+JAVA_OPTIONS="$JAVA_OPTIONS -javaagent:/elastic-apm-agent.jar"
+JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.application_packages=com.github.olly.workshop"
+
 if [ "$JAVA_DEBUG" = true ]; then
   JAVA_OPTIONS="$JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005";
 fi
