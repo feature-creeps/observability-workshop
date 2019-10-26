2435108333b6ea305f79f2dfb20d8a4f5f7e9e64
Abby Bangser <abby.bangser@moo.com>
2019-10-26 15:36:24 +0100

    removing apm to make logs on disk less chatty

--- stack/services/imageflip/src/main/docker/entrypoint.sh
+++ stack/services/imageflip/src/main/docker/entrypoint.sh
@@ -4,7 +4,7 @@ JAVA_OPTIONS="$JAVA_OPTIONS -Djava.security.egd=file:/dev/./urandom"
 JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.service_name=imageflip"
 JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.server_urls=http://apm-server:8200"
 JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.enable_log_correlation=true"
-JAVA_OPTIONS="$JAVA_OPTIONS -javaagent:/elastic-apm-agent.jar"
+# JAVA_OPTIONS="$JAVA_OPTIONS -javaagent:/elastic-apm-agent.jar"
 JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.application_packages=com.github.olly.workshop"
 
 if [ "$JAVA_DEBUG" = true ]; then
