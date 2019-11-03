731be92b0f90a687746763e20aee59ad5ba528fe
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2019-11-03 21:36:39 +0100

    deactivate APM for the first 6 levels

--- stack/services/imagethumbnail/src/main/docker/entrypoint.sh
+++ stack/services/imagethumbnail/src/main/docker/entrypoint.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 JAVA_OPTIONS=""
 JAVA_OPTIONS="$JAVA_OPTIONS -Djava.security.egd=file:/dev/./urandom"
-JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.service_name=imagethumbnail"
-JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.server_urls=http://apm-server:8200"
-JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.enable_log_correlation=true"
-JAVA_OPTIONS="$JAVA_OPTIONS -javaagent:/elastic-apm-agent.jar"
-JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.application_packages=com.github.olly.workshop"
+
+if [ "$APM_ENABLED" = true ]; then
+    JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.service_name=imagethumbnail"
+    JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.server_urls=http://apm-server:8200"
+    JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.enable_log_correlation=true"
+    JAVA_OPTIONS="$JAVA_OPTIONS -javaagent:/elastic-apm-agent.jar"
+    JAVA_OPTIONS="$JAVA_OPTIONS -Delastic.apm.application_packages=com.github.olly.workshop"
+fi
 
 if [ "$JAVA_DEBUG" = true ]; then
   JAVA_OPTIONS="$JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005";
