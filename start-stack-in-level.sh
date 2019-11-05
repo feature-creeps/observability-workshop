9ef5f4c1829f6ab6f5c803ced06afe6ae514305a
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2019-11-05 12:05:55 +0100

    rebuild on level startup

--- start-stack-in-level.sh
+++ start-stack-in-level.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 lvl=$1
 cd stack/stack-full/
-docker-compose -f docker-compose-level-$lvl.yml up -d --remove-orphans
+docker-compose -f docker-compose-level-$lvl.yml up -d --build --remove-orphans
