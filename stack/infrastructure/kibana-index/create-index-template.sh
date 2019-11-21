495b865bed35672b683c89f8392342ec145ab8d3
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2019-11-21 11:06:18 +0100

    added index template script

---
+++ stack/infrastructure/kibana-index/create-index-template.sh
@@ -0,0 +1,30 @@
+#!/binbash
+
+curl -X PUT "http://elastic:changeme@elasticsearch:9200/_template/logs?pretty" -H 'Content-Type: application/json' -d'
+{
+  "index_patterns": ["logs-*"],
+  "settings": {
+    "number_of_shards": 1
+  },
+  "mappings": {
+    "properties": {
+      "degrees":    { "type": "long" },
+      "factor":    { "type": "long" },
+      "horizontal":    { "type": "boolean" },
+      "vertical":    { "type": "boolean" },
+      "span.duration_ms":    { "type": "integer" },
+      "span.content_size":    { "type": "integer" },
+      "span.content_size_new":    { "type": "integer" },
+      "span.content_size_original":    { "type": "integer" },
+      "span.finishedAt":    { "type": "date" },
+      "span.response_status":    { "type": "integer" },
+      "span.startedAt":    { "type": "date" },
+      "span.tranformation_flip":    { "type": "boolean" },
+      "span.tranformation_greyscale":    { "type": "boolean" },
+      "span.tranformation_image_persist":    { "type": "boolean" },
+      "span.tranformation_resize":    { "type": "boolean" },
+      "span.tranformation_rotate":    { "type": "boolean" }
+    }
+  }
+}
+'
