9075840df4dccde0f40be7bce7cede110df70bc2
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2020-01-08 17:03:06 +0100

    access backend services via proxy path

--- stack/application/frontend/src/environments/environment.prod.ts
+++ stack/application/frontend/src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   backend: {
-    imageorchestrator: 'http://46.101.170.74:8080',
-    imageholder: 'http://46.101.170.74:8081',
-    imagerotator: 'http://46.101.170.74:8082',
-    imagegrayscale: 'http://46.101.170.74:8083',
-    imageresize: 'http://46.101.170.74:8084',
-    imageflip: 'http://46.101.170.74:8085',
-    imagethumbnail: 'http://46.101.170.74:8086'
+    imageorchestrator: 'http://46.101.170.74/proxy/imageorchestrator',
+    imageholder: 'http://46.101.170.74/proxy/imageholder',
+    imagerotator: 'http://46.101.170.74/proxy/imagerotator',
+    imagegrayscale: 'http://46.101.170.74/proxy/imagegrayscale',
+    imageresize: 'http://46.101.170.74/proxy/imageresize',
+    imageflip: 'http://46.101.170.74/proxy/imageflip',
+    imagethumbnail: 'http://46.101.170.74/proxy/imagethumbnail'
   },
   tools: {
     kibana: 'http://46.101.170.74:5601',
