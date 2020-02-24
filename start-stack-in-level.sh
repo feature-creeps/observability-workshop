04ca427c7feb189baf76cab9017820ce08de1cc7
Abby Bangser <abby.bangser@moo.com>
2020-02-24 18:06:42 +0000

    add deployment markers for ./start-{}-in-level.sh

--- start-stack-in-level.sh
+++ start-stack-in-level.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 lvl=$1
 cd $HOME/observability-workshop/stack/compose/
-docker-compose -f docker-compose-level-$lvl.yml up -d --build --remove-orphans
+START=`date +%s`
+docker-compose -f docker-compose-level-$lvl.yml --compatibility up -d --build --remove-orphans
+
+END=`date +%s`
+curl https://api.honeycomb.io/1/markers/$HONEYCOMB_DATASET -X POST  \
+    -H "X-Honeycomb-Team: $HONEYCOMB_KEY"  \
+    -d "{\"message\":\"start-stack $lvl\", \"type\":\"deploy\", \"start_time\": $START, \"end_time\": $END}"
--- start-swarm-in-level.sh
+++ start-swarm-in-level.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 lvl=$1
 cd $HOME/observability-workshop/stack/compose/
-docker-compose -f docker-swarm-level-$lvl.yml build
+
 docker-compose -f docker-swarm-level-$lvl.yml pull
+docker-compose -f docker-swarm-level-$lvl.yml build
 docker-compose -f docker-swarm-level-$lvl.yml push
+
+START=`date +%s`
 docker stack deploy --compose-file docker-swarm-level-$lvl.yml --prune o11ystack
+
+END=`date +%s`
+curl https://api.honeycomb.io/1/markers/$HONEYCOMB_DATASET -X POST  \
+    -H "X-Honeycomb-Team: $HONEYCOMB_KEY"  \
+    -d "{\"message\":\"start-stack $lvl\", \"type\":\"deploy\", \"start_time\": $START, \"end_time\": $END}"
