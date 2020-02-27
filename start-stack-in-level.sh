7c83ee30f9231cabb9cfb894b0c343130f2a6146
Abby Bangser <abby.bangser@moo.com>
2020-02-27 18:52:59 +0000

    fix var for honeycomb marker

--- start-stack-in-level.sh
+++ start-stack-in-level.sh
@@ -5,6 +5,6 @@ START=`date +%s`
 docker-compose -f docker-compose-level-$lvl.yml --compatibility up -d --build --remove-orphans
 
 END=`date +%s`
-curl https://api.honeycomb.io/1/markers/$HONEYCOMB_DATASET -X POST  \
+curl https://api.honeycomb.io/1/markers/${HONEYCOMB_DATASET} -X POST  \
     -H "X-Honeycomb-Team: $HONEYCOMB_KEY"  \
     -d "{\"message\":\"start-stack $lvl\", \"type\":\"deploy\", \"start_time\": $START, \"end_time\": $END}"
