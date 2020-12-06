6c56ae18b5c39989338a27aebd72f0f829f8863a
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2020-12-06 22:21:05 +0100

    fixed debug param in entrypoint

--- stack/application/imagegrayscale/src/main/docker/entrypoint.sh
+++ stack/application/imagegrayscale/src/main/docker/entrypoint.sh
@@ -11,7 +11,7 @@ if [ "$APM_ENABLED" = true ]; then
 fi
 
 if [ "$JAVA_DEBUG" = true ]; then
-  JAVA_OPTIONS="$JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005";
+  JAVA_OPTIONS="$JAVA_OPTIONS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005";
 fi
 
 exec java $JAVA_OPTIONS -jar /imagegrayscale.jar $SPRING_OPTIONS "$@"
