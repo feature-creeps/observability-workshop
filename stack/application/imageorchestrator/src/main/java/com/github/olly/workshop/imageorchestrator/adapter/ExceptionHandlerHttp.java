73ace67671547ab2b2cdfad1b649a1f59c38c704
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2020-12-11 18:22:50 +0100

    return response entity without ex, append ex to event

--- stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/adapter/ExceptionHandlerHttp.java
+++ stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/adapter/ExceptionHandlerHttp.java
@@ -1,5 +1,7 @@
 package com.github.olly.workshop.imageorchestrator.adapter;
 
+import com.github.olly.workshop.imageorchestrator.service.EventService;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -7,27 +9,41 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 @ControllerAdvice
 public class ExceptionHandlerHttp {
 
+  private final EventService eventService;
+
   private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerHttp.class);
 
+  public ExceptionHandlerHttp(
+      EventService eventService) {
+    this.eventService = eventService;
+  }
+
   @ExceptionHandler({NotFoundException.class})
-  @ResponseStatus(HttpStatus.NOT_FOUND)
   @ResponseBody
   ResponseEntity<Exception> handleNotFoundException(Exception ex) throws Exception {
-    LOGGER.error(ex.getLocalizedMessage(), ex);
-    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    logAndAppendToEvent(ex);
+    return new ResponseEntity<>(((ResponseStatusException) ex).getStatus());
   }
 
   @ExceptionHandler({Throwable.class})
-  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ResponseBody
   ResponseEntity<Exception> handleThrowable(Exception ex) throws Exception {
-    LOGGER.error(ex.getLocalizedMessage(), ex);
-    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    ex = new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "unexpected exception thrown", ex);
+    logAndAppendToEvent(ex);
+    return new ResponseEntity<>(((ResponseStatusException) ex).getStatus());
   }
 
+  private void logAndAppendToEvent(Throwable ex) {
+    LOGGER.error(ex.getLocalizedMessage(), ex);
+
+    eventService.addFieldToActiveEvent("response_status", ((ResponseStatusException) ex).getStatus());
+    eventService.addFieldToActiveEvent("exception_thrown", "true");
+    eventService.addFieldToActiveEvent("exception_message", ex.getMessage());
+    eventService.addFieldToActiveEvent("exception_stacktrace", ExceptionUtils.getStackTrace(ex));
+  }
 }
