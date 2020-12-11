228288af3c123d66f25dee0d878107189852a8de
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2020-12-11 18:12:46 +0100

    return response entity without ex, log ex

--- stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/adapter/ExceptionHandlerHttp.java
+++ stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/adapter/ExceptionHandlerHttp.java
@@ -1,5 +1,7 @@
 package com.github.olly.workshop.imageorchestrator.adapter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,18 +12,22 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class ExceptionHandlerHttp {
 
+  private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerHttp.class);
+
   @ExceptionHandler({NotFoundException.class})
   @ResponseStatus(HttpStatus.NOT_FOUND)
   @ResponseBody
   ResponseEntity<Exception> handleNotFoundException(Exception ex) throws Exception {
-    return new ResponseEntity<>(ex, HttpStatus.NOT_FOUND);
+    LOGGER.error(ex.getLocalizedMessage(), ex);
+    return new ResponseEntity<>(HttpStatus.NOT_FOUND);
   }
 
   @ExceptionHandler({Throwable.class})
   @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
   @ResponseBody
   ResponseEntity<Exception> handleThrowable(Exception ex) throws Exception {
-    return new ResponseEntity<>(ex, HttpStatus.INTERNAL_SERVER_ERROR);
+    LOGGER.error(ex.getLocalizedMessage(), ex);
+    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
   }
 
 }
