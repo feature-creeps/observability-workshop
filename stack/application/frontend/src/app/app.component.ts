c1addf3823890c74de629a01daff24d8b76265b0
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2020-12-12 19:50:20 +0100

    implement OnInit

--- stack/application/frontend/src/app/app.component.ts
+++ stack/application/frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DIMa: Distributed Image Manipulation';
 
   ngOnInit(): void {
