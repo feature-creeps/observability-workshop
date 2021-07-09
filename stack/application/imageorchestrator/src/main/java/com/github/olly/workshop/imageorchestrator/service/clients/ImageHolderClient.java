7d5e4616c2fa2da56ad445651244ffcb3313aa3d
Marcel Henrich <marcel.henrich@maibornwolff.de>
2021-07-09 15:00:11 +0200

    add  -service to ImageHolderClient

--- stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/service/clients/ImageHolderClient.java
+++ stack/application/imageorchestrator/src/main/java/com/github/olly/workshop/imageorchestrator/service/clients/ImageHolderClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "imageholder", url = "imageholder:8080")
+@FeignClient(value = "imageholder", url = "imageholder-service:8080")
 public interface ImageHolderClient {
 
     @RequestMapping(method = RequestMethod.GET, value = "api/images/{id}")
