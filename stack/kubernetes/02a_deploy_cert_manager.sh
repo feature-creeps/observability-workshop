401218f41a547eca7423eb70660719df53a3b18d
Marcel Henrich <marcel.henrich@maibornwolff.de>
2021-12-23 13:01:18 +0100

    integrate cert-manager + letsencrypt for https

---
+++ stack/kubernetes/02a_deploy_cert_manager.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+NAMESPACE="cert-manager"
+VERSION="v1.6.1"
+
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+
+helm upgrade --install \
+  cert-manager jetstack/cert-manager \
+  --namespace "$NAMESPACE" \
+  --create-namespace \
+  --version "$VERSION" \
+  --set installCRDs=true
+
+kubectl apply -f tools/cert-manager/letsencrypt_prod.yaml -n "$NAMESPACE"
--- stack/kubernetes/04_deploy_dima.sh
+++ stack/kubernetes/04_deploy_dima.sh
@@ -5,7 +5,7 @@ NAMESPACE=dima
 
 # create dima namespace
 kubectl get namespace "$NAMESPACE" || kubectl create namespace "$NAMESPACE"
-kubectl label namespace "$NAMESPACE" istio-injection=enabled
+kubectl label namespace "$NAMESPACE" istio-injection=enabled --overwrite
 
 # deploy application services & traffic gen
 kustomize build application | kubectl -n "$NAMESPACE" apply -f -
