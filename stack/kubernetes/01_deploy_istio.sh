f3f13f9c5158a5110a1a36a79a96b869fd707520
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2021-12-24 12:00:48 +0100

    added grafana auth

--- stack/kubernetes/01_deploy_istio.sh
+++ stack/kubernetes/01_deploy_istio.sh
@@ -5,7 +5,15 @@ set -eu -o pipefail
 istioctl install --set profile=demo -y
 
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
-kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/grafana.yaml
+
+# grafana
+kubectl apply -f tools/istio/istio-grafana.yaml
+
+# kiali
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/kiali.yaml
+
+# prometheus
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/prometheus.yaml
+
+# jaeger
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/jaeger.yaml
