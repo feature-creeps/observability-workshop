ac30f33a3f41cf696f712c08de1708922429d5b5
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2021-12-24 20:43:19 +0100

    reduce istio to minimal setup, reduce elasticsearch to 2 replicas

--- stack/kubernetes/01_deploy_istio.sh
+++ stack/kubernetes/01_deploy_istio.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -eu -o pipefail
 
-# todo: why use profile demo?
-istioctl install --set profile=demo -y
+# minimal profile does not deploy egress/ingress, see https://istio.io/latest/docs/setup/additional-setup/config-profiles/
+istioctl install --set profile=minimal -y
 
 kubectl create clusterrolebinding cluster-admin-binding --clusterrole cluster-admin --user $(gcloud config get-value account)
 
