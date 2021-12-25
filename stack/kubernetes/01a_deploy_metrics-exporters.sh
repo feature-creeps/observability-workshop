19d0d3813800c15e5803fbf6b980c18ac50081f6
Benjamin Hofmann <benjamin.hofmann@maibornwolff.de>
2021-12-25 21:11:30 +0100

    added kube state metrics exporter

--- stack/kubernetes/01a_deploy_metrics-exporters.sh
+++ stack/kubernetes/01a_deploy_metrics-exporters.sh
@@ -6,6 +6,10 @@ NAMESPACE=monitoring
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
+# node exporter
 helm upgrade --install -f "tools/node-exporter/values.yaml" -n "$NAMESPACE" --create-namespace node-exporter prometheus-community/prometheus-node-exporter
+
+# kube state metrics
+helm upgrade --install -f "tools/kube-state-metrics/values.yaml" -n "$NAMESPACE" --create-namespace kube-state-metrics prometheus-community/kube-state-metrics
 
 kubectl label namespace "$NAMESPACE" istio-injection=enabled --overwrite
