22d9da27ba5d3a1ef32dc710fe1eda4ca51b3c2f
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-02-23 10:20:02 +0100

    Create IAM for github

--- stack/kubernetes/00a_setup_iam.sh
+++ stack/kubernetes/00a_setup_iam.sh
@@ -1,9 +1,27 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-SA_NAME=${1:-"github-actions"}
-SA_EMAIL="${SA_NAME}@olly-2021-k8s-migration.iam.gserviceaccount.com"
+# iam for github actions, see https://github.com/google-github-actions/auth
+
 GKE_PROJECT="olly-2021-k8s-migration"
+WIP="github-pool"
+WIP_PROVIDER="github-provider"
+SA_NAME=${1:-"github-actions"}
+SA_EMAIL="${SA_NAME}@${GKE_PROJECT}.iam.gserviceaccount.com"
+
+gcloud iam workload-identity-pools create "${WIP}" \
+  --project="${GKE_PROJECT}" \
+  --location="global" \
+  --display-name="Github pool"
+
+gcloud iam workload-identity-pools providers create-oidc "${WIP_PROVIDER}" \
+  --project="${GKE_PROJECT}" \
+  --location="global" \
+  --workload-identity-pool="${WIP}" \
+  --display-name="Github provider" \
+  --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.aud=assertion.aud" \
+  --attribute-mapping="google.subject=assertion.sub,attribute.repository=assertion.repository" \
+  --issuer-uri="https://token.actions.githubusercontent.com"
 
 gcloud iam service-accounts create "$SA_NAME"
 
@@ -11,6 +29,13 @@ gcloud projects add-iam-policy-binding "$GKE_PROJECT" \
   --member=serviceAccount:"$SA_EMAIL" \
   --role=roles/container.admin \
   --role=roles/storage.admin
+
+gcloud iam service-accounts add-iam-policy-binding "${SA_EMAIL}" \
+  --project="${GKE_PROJECT}" \
+  --role=roles/container.admin \
+  --role=roles/storage.admin \
+  --role="roles/iam.workloadIdentityUser" \
+  --member="principalSet://iam.googleapis.com/projects/399309401465/locations/global/workloadIdentityPools/${WIP}/attribute.repository/feature-creeps/observability-workshop"
 
 gcloud iam service-accounts keys create key.json --iam-account="$SA_EMAIL"
 
