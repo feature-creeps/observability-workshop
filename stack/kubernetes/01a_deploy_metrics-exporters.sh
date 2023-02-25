eed8208eea20cd04694178f3303e34ee74fe6c80
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-02-25 18:49:41 +0100

    Create custom grafana dashboards

--- stack/kubernetes/01a_deploy_metrics-exporters.sh
+++ stack/kubernetes/01a_deploy_metrics-exporters.sh
@@ -3,17 +3,22 @@ set -eu -o pipefail
 
 NAMESPACE=monitoring
 
+echo "--- install and update helm repos"
+
 helm repo add grafana https://grafana.github.io/helm-charts
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 
-# prometheus
+echo "--- install prometheus"
 helm upgrade --install -f "tools/prometheus/values.yaml" -n "$NAMESPACE" --create-namespace prometheus prometheus-community/prometheus
 
-# grafana
+echo "--- install grafana"
 helm upgrade --install -f "tools/grafana/values.yaml" -n "$NAMESPACE" --create-namespace grafana grafana/grafana
 
-# jaeger
+echo "--- create grafana dashboards through configmaps"
+kustomize build tools/grafana/dashboards | kubectl apply -f -
+
+echo "--- install jaeger"
 kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.12/samples/addons/jaeger.yaml
 
 kubectl label namespace "$NAMESPACE" istio-injection=enabled --overwrite
