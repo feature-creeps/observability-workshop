f5e24e8e775186f9f9001b339f20dd0467b6edde
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-01 14:27:47 +0100

    Initial helm deployment

--- stack/kubernetes/16_deploy_dima.sh
+++ stack/kubernetes/16_deploy_dima.sh
@@ -2,12 +2,13 @@
 set -euo pipefail
 
 NAMESPACE=dima
+IMAGE_TAG=${1:-"latest"}
 
-echo "=== Deploy application stack in namespace ${NAMESPACE}"
+echo "=== Deploy application stack with image tag ${IMAGE_TAG} in namespace ${NAMESPACE}"
 
 echo "--- create dima namespace"
 kubectl get namespace "$NAMESPACE" || kubectl create namespace "$NAMESPACE"
 kubectl label namespace "$NAMESPACE" istio-injection=enabled --overwrite
 
 echo "--- deploy dima"
-helm upgrade --install -n "$NAMESPACE" dima application
+helm upgrade --install -n "$NAMESPACE" --set global.image.tag=$IMAGE_TAG dima application
