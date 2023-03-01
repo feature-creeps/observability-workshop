20dfdeee9873e801799b4b3e804b7c425d05d196
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-01 12:42:01 +0100

    Initial helm deployment

--- stack/kubernetes/16_deploy_dima.sh
+++ stack/kubernetes/16_deploy_dima.sh
@@ -9,6 +9,5 @@ echo "--- create dima namespace"
 kubectl get namespace "$NAMESPACE" || kubectl create namespace "$NAMESPACE"
 kubectl label namespace "$NAMESPACE" istio-injection=enabled --overwrite
 
-echo "--- deploy application services & traffic gen"
-kubectl delete job -n "$NAMESPACE" --ignore-not-found traffic-gen-traffic-gen
-kustomize build application | kubectl -n "$NAMESPACE" apply -f -
+echo "--- deploy dima"
+helm upgrade --install -n "$NAMESPACE" dima application
