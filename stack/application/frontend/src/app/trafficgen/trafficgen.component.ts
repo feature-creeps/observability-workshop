5976642b46e332374fd49488dbac0c98b5eef189
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-07 10:15:11 +0100

    Add trafficgen to UI

--- stack/application/frontend/src/app/trafficgen/trafficgen.component.ts
+++ stack/application/frontend/src/app/trafficgen/trafficgen.component.ts
@@ -8,33 +8,51 @@ import { environment } from "../../environments/environment";
   styleUrls: ['./trafficgen.component.css']
 })
 
-export class TrafficGeneratorComponent implements OnInit {
+export class TrafficGeneratorComponent {
 
   constructor(private http: HttpClient) {
   }
 
-  ngOnInit(): void {
-  }
-
   async uploadAll() {
-    let res;
     try {
-      res = await this.http.post(environment.backend.trafficgen + '/api/traffic/image/upload', null, { responseType: 'text' }).toPromise();
+      await this.http.post(environment.backend.trafficgen + '/api/traffic/image/upload', null, { responseType: 'text' }).toPromise();
+      TrafficGeneratorComponent.info("Upload of all images triggered successfully.", InfoType.success);
     } catch (e) {
-      let info = document.getElementById("info");
-      info.innerText = "Failed to trigger upload of all images";
-      info.className = "btn btn-block btn-danger dima-btn"
+      console.log(e)
+      TrafficGeneratorComponent.info("Failed to trigger upload of all images", InfoType.danger);
     }
   }
 
   async sendTransformationRequest(formInput: any) {
-    let res;
     try {
-      res = await this.http.post(environment.backend.trafficgen + '/api/traffic/image/transform/start?transformationsPerSecond=' + formInput.transformationsPerSecond, null, { responseType: 'text' }).toPromise();
+      await this.http.post(environment.backend.trafficgen + '/api/traffic/image/transform/start?transformationsPerSecond=' + formInput.transformationsPerSecond, null, { responseType: 'text' }).toPromise();
+      TrafficGeneratorComponent.info("Transformation traffic request sent successfully.", InfoType.success);
     } catch (e) {
-      let info = document.getElementById("info");
-      info.innerText = "Failed to trigger upload of all images";
-      info.className = "btn btn-block btn-danger dima-btn"
+      console.log(e)
+      TrafficGeneratorComponent.info("Failed to send transformation traffic request", InfoType.danger);
     }
   }
+
+  async stopTransformationTraffic() {
+    try {
+      await this.http.post(environment.backend.trafficgen + '/api/traffic/image/transform/stop', null, { responseType: 'text' }).toPromise();
+      TrafficGeneratorComponent.info("Stop transformation request sent successfully.", InfoType.success);
+    } catch (e) {
+      console.log(e)
+      TrafficGeneratorComponent.info("Failed to send stop transformation traffic request", InfoType.danger);
+    }
+  }
+
+  private static info(text: string, type: InfoType) {
+    let info = <HTMLInputElement>document.getElementById("info");
+    info.hidden = false
+    info.value = text
+    info.className = "fade-in btn-block btn-" + InfoType[type] + " dima-btn"
+  }
+}
+
+enum InfoType {
+  warning,
+  danger,
+  success
 }
