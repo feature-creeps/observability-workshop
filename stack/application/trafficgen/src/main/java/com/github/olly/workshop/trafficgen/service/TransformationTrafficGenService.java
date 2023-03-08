5d65ae100ec0afa61730ead717ac07326e515191
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-08 11:48:17 +0100

    Improve Trafficgen

--- stack/application/trafficgen/src/main/java/com/github/olly/workshop/trafficgen/service/TransformationTrafficGenService.java
+++ stack/application/trafficgen/src/main/java/com/github/olly/workshop/trafficgen/service/TransformationTrafficGenService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Async;
 
 @Service
@@ -12,6 +14,9 @@ public class TransformationTrafficGenService {
     @Autowired
     private RandomTransformationService randomTransformationService;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TransformationTrafficGenService.class);
+    private static final long S_IN_MS = 1000L;
+
     private int transformationsPerSecond = 0;
 
     public void setTransformationsPerSecond(int transformationsPerSecond) {
@@ -19,10 +24,15 @@ public class TransformationTrafficGenService {
     }
 
     @Async
-    @Scheduled(fixedRate = 1000)
+    @Scheduled(fixedRate = S_IN_MS)
     public void generateTraffic() {
         for (int i = 0; i < transformationsPerSecond; i++) {
             randomTransformationService.sendRandomRequest();
+            try {
+                wait(S_IN_MS / transformationsPerSecond);
+            } catch (InterruptedException e) {
+                LOGGER.warn("Interrupted waiting", e);
+            }
         }
     }
 }
