c81b42dac50a7d09eb8609470fbe5106fe5e4595
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-13 12:44:00 +0100

    Delete deprecated files

--- stack/compose/archived/restart_business.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-/usr/local/bin/docker-compose --project-directory /home/olly/observability-workshop/stack/compose/ \
-        -f /home/olly/observability-workshop/stack/compose/docker-compose-level-9.yml \
-        up --build -d 2>&1
