26020fed36e4654e4c7b1608482289b264384acb
Dominik Ufer <dominik.ufer@maibornwolff.de>
2023-03-13 17:08:10 +0100

    Make nginx parameters configurable

--- local/30_build.sh
+++ local/30_build.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 export GKE_REGISTRY=local
 export GITHUB_SHA="$(git rev-parse --short HEAD)-snapshot"
 
-cd ../stack/compose
-docker compose -f docker-compose.yml -f docker-compose-registry-tags.yml build --parallel
+docker compose -f ../stack/compose/docker-compose.yml -f ../stack/compose/docker-compose-registry-tags.yml build --parallel
