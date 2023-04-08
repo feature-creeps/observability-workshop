b3545688aee1fc4683a9048afb061fb466e7aae9
Luis Schweigard <luis.schweigard@maibornwolff.de>
2023-04-08 22:16:29 +0200

    Preparing local setup with k3d and ingress

---
+++ local/kubernetes/00_setup_k3d.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+CLUSTER_NAME=o11y-stack
+
+echo "Setting up k3d cluster '$CLUSTER_NAME'"
+
+if k3d cluster list | grep $CLUSTER_NAME > /dev/null; then
+    echo "Cluster already exists, exiting ..."
+    exit 1
+fi
+
+k3d cluster create $CLUSTER_NAME \
+    -p "8080:80@loadbalancer" \
+    --k3s-arg "--disable=traefik@server:0" \
+    --agents 0
+
