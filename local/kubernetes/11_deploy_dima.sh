ca06200fb1fdf4d3327c0c70059dd3b2a20bffab
Luis Schweigard <luis.schweigard@maibornwolff.de>
2023-04-17 10:53:07 +0200

    Adding readme and k3d image import

--- local/kubernetes/11_deploy_dima.sh
+++ local/kubernetes/11_deploy_dima.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+REGISTRY=local
 NAMESPACE=dima
 IMAGE_TAG="$(git rev-parse --short HEAD)-snapshot"
+CLUSTER_NAME=o11y-stack
 
 echo "=== Deploy application stack with image tag ${IMAGE_TAG} in namespace ${NAMESPACE}"
+
+echo "--- import images into k3d"
+for app in frontend imageorchestrator imageholder imagerotator imagegrayscale imageresize imageflip imagethumbnail trafficgen; do
+    k3d image import -c "$CLUSTER_NAME" "$REGISTRY/dima-$app:$IMAGE_TAG"
+done
 
 echo "--- create dima namespace"
 kubectl get namespace "$NAMESPACE" || kubectl create namespace "$NAMESPACE"
